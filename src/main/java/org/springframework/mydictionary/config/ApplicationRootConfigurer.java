757cab66454e8c816fc71f73ba1ea00183b298cf
dingding <ruixue.fi@gmail.com>
2017-08-30 17:41:39 +0300

    configuration Java added

--- src/main/java/org/springframework/mydictionary/config/ApplicationRootConfigurer.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.mydictionary.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ApplicationRootConfigurer {
-
-}
---
+++ src/main/java/org/springframework/mydictionary/config/WebInitializer.java
@@ -0,0 +1,21 @@
+package org.springframework.mydictionary.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[]{RootConfigurer.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+
+}
