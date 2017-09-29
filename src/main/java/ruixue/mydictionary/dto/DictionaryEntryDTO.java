0c7d039e9bd09a0366157fda13c17cf533be4746
dingding <ruixue.fi@gmail.com>
2017-09-29 19:58:30 +0300

    DTO updated

--- src/main/java/ruixue/mydictionary/dto/DictionaryEntryDTO.java
+++ src/main/java/ruixue/mydictionary/dto/DictionaryEntryDTO.java
@@ -2,10 +2,6 @@ package ruixue.mydictionary.dto;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import ruixue.mydictionary.entity.Explanation;
-import ruixue.mydictionary.entity.ExplanationMapping;
 
 public class DictionaryEntryDTO {
 	private String language;
@@ -13,16 +9,6 @@ public class DictionaryEntryDTO {
 	private String explainedLanguage;
 	private Map<String, List<String>> explanations;
 	//DTO design pattern
-	public DictionaryEntryDTO(ExplanationMapping explanationMapping) {
-		this (explanationMapping.getWord().getLanguage().getLanguage(),
-				explanationMapping.getWord().getWord(),
-				explanationMapping.getExplanationLanguage().getLanguage(),
-				explanationMapping.getExplanations().stream().collect(
-						Collectors.groupingBy(
-								e -> e.getPartOfSpeech().getPartOfSpeech(), 
-								Collectors.mapping(Explanation::getExplanation, Collectors.toList())
-						)));
-	}
 	public DictionaryEntryDTO(String language, String word, String explainedLanguage, Map<String, List<String>> explanations) {
 		this.language = language;
 		this.word = word;
