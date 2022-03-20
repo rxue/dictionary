25327a7b4cbef05a6d68eada9aa78c33f7fc8b8a
rui <ruixue.fi@gmail.com>
2022-03-20 12:06:30 +0200

    #1 parameter for search added

--- web/src/main/java/rx/dictionary/application/SearchServlet.java
+++ web/src/main/java/rx/dictionary/application/SearchServlet.java
@@ -11,30 +11,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+
 @WebServlet(name="search", urlPatterns="/search")
 public class SearchServlet extends HttpServlet {
 	@Override
-	protected void	doGet(HttpServletRequest req, HttpServletResponse resp) { 
-		resp.setContentType("image/jpeg");
-		ServletContext ctx = getServletContext();
-		try(OutputStream os = resp.getOutputStream();
-				InputStream is = ctx.getResourceAsStream("resources/img/bucket.jpg")) {
-			//long bytes = Files.copy(Paths.get("resources", "img","bucket.jpg"), os);
-	        transfer(is, os);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	protected void	doGet(HttpServletRequest req, HttpServletResponse resp) {
+		String word = req.getParameter("word");
+		if (!StringUtils.isEmpty(word)) {
+			resp.setContentType("image/jpeg");
+			ServletContext ctx = getServletContext();
+			try(OutputStream os = resp.getOutputStream();
+					InputStream is = ctx.getResourceAsStream(getImagePath(word))) {
+		        transfer(is, os);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+	}
+	private static String getImagePath(String word) {
+		return "resources/img/" + word + ".jpg";
 	}
 	
 	/**
 	 * Mostly copied from https://github.com/openjdk/jdk/blob/master/src/java.base/share/classes/java/io/InputStream.java 
 	 * 
+	 * => once code updated to Java 9 or higher, this can be fully replaced by InputStream.transfer, which is since Java 9
+	 * 
 	 * @param in
 	 * @param out
 	 * @throws IOException
 	 */
-	private void transfer(InputStream in, OutputStream out) throws IOException {
+	private static long transfer(InputStream in, OutputStream out) throws IOException {
 		final int defaultBufferSize = 8192;
 		Objects.requireNonNull(out, "out");
         long transferred = 0;
@@ -44,5 +53,6 @@ public class SearchServlet extends HttpServlet {
             out.write(buffer, 0, read);
             transferred += read;
         }
+        return transferred;
 	}
 }
