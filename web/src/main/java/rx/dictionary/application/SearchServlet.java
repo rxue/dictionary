6cbcc5b5d7280f632ec39615ccf803a299343fb7
rui <ruixue.fi@gmail.com>
2022-03-20 08:01:26 +0200

    #1 forced compiler to compile merely on base of Java 8 through mave-compiler-plugin
     NOTE! maven-compiler-plugin version has to be 3.10+ based on https://maven.apache.org/plugins/maven-compiler-plugin/examples/set-compiler-release.html

--- web/src/main/java/rx/dictionary/application/SearchServlet.java
+++ web/src/main/java/rx/dictionary/application/SearchServlet.java
@@ -3,6 +3,7 @@ package rx.dictionary.application;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Objects;
 
 import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebServlet;
@@ -18,13 +19,30 @@ public class SearchServlet extends HttpServlet {
 		ServletContext ctx = getServletContext();
 		try(OutputStream os = resp.getOutputStream();
 				InputStream is = ctx.getResourceAsStream("resources/img/bucket.jpg")) {
-	        byte[] allBytes = is.readAllBytes();
 			//long bytes = Files.copy(Paths.get("resources", "img","bucket.jpg"), os);
-	        os.write(allBytes, 0, allBytes.length);
-			System.out.println("Number of bytes read and wrote is " + allBytes.length);
+	        transfer(is, os);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Mostly copied from https://github.com/openjdk/jdk/blob/master/src/java.base/share/classes/java/io/InputStream.java 
+	 * 
+	 * @param in
+	 * @param out
+	 * @throws IOException
+	 */
+	private void transfer(InputStream in, OutputStream out) throws IOException {
+		final int defaultBufferSize = 8192;
+		Objects.requireNonNull(out, "out");
+        long transferred = 0;
+        byte[] buffer = new byte[defaultBufferSize];
+        int read;
+        while ((read = in.read(buffer, 0, defaultBufferSize)) >= 0) {
+            out.write(buffer, 0, read);
+            transferred += read;
+        }
 	}
 }
