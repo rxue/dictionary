26ee3b5dc41a62b2fe9cb9fc0e5d7d08f2c47906
rui <ruixue.fi@gmail.com>
2022-03-31 21:23:08 +0300

    #4 resolved by using h:commandButton

--- web/src/main/java/rx/dictionary/application/Dictionary.java
+++ web/src/main/java/rx/dictionary/application/Dictionary.java
@@ -8,16 +8,23 @@ import javax.inject.Named;
 @Named
 public class Dictionary {
 	private String keyword;
+	private boolean hasImage;
 	public void search() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		System.out.println("when being invoked, the current phase is " + context.getCurrentPhaseId());
 		System.out.println("search word: " + keyword);
+		hasImage = true;
 	}
 	public void setKeyword(String keyword) {
-		System.out.println("The set keyword is " + keyword);
 		this.keyword = keyword;
 	}
 	public String getKeyword() {
 		return keyword;
+	}
+	public boolean getHasImage() {
+		return hasImage;
+	}
+	public String getImage() {
+		return "https://peda.net/aanekoski/p%C3%A4iv%C3%A4hoito/mp/maahiset2/maahiset2/aurinko-jpg:file/photo/40880a3811061a75dfc8373970b0aa45dbd45519/aurinko.jpg";
 	}
 }
