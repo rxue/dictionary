e052339b91eba283c4330c7072f9c9c31bd66bd2
rui <ruixue.fi@gmail.com>
2022-04-02 21:02:32 +0300

    #4 image display logic works

--- web/src/main/java/rx/dictionary/application/Dictionary.java
+++ web/src/main/java/rx/dictionary/application/Dictionary.java
@@ -8,24 +8,22 @@ import javax.inject.Named;
 @Named
 public class Dictionary {
 	private String keyword;
-	private boolean hasImage;
+	private String imageUrl = "https://www.dreameant.com/wp-content/uploads/2021/07/Sun-Dream-Interpretation.jpg";
 	public void search() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		System.out.println("when being invoked, the current phase is " + context.getCurrentPhaseId());
 		System.out.println("search word: " + keyword);
-		hasImage = true;
 	}
 	public void setKeyword(String keyword) {
-		System.out.println("set keyword: " + keyword);
 		this.keyword = keyword;
 	}
 	public String getKeyword() {
 		return keyword;
 	}
 	public boolean getHasImage() {
-		return hasImage;
+		return keyword != null;
 	}
-	public String getImage() {
-		return "https://peda.net/aanekoski/p%C3%A4iv%C3%A4hoito/mp/maahiset2/maahiset2/aurinko-jpg:file/photo/40880a3811061a75dfc8373970b0aa45dbd45519/aurinko.jpg";
+	public String getImageUrl() {
+		return imageUrl;
 	}
 }
