111a8dfb8d1961b75dcf1384ad5ce0b18a6eeac1
rui <ruixue.fi@gmail.com>
2022-05-29 08:30:15 +0300

    #22 complete set of docker configuration files added
     - templates dir removed for simplicity

--- docker/run_web.sh
+++ docker/run_web.sh
@@ -1,8 +1,4 @@
 mvn -f ../web/pom.xml clean package
-if [ ! -d "../web/docker_config" ]; then
-  mkdir ../web/docker_config
-  cp templates/standalone.xml ../web/docker_config
-fi
 docker-compose up -d --build web
 docker-compose logs -f web
 
