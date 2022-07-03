edd5cdc97efb538ac918021015755087ff08c4d1
rui <ruixue.fi@gmail.com>
2022-07-03 22:44:50 +0300

    #32 failed trial

--- backoffice/src/main/java/rx/backoffice/DataExportBean.java
+++ backoffice/src/main/java/rx/backoffice/DataExportBean.java
@@ -1,10 +1,23 @@
 package rx.backoffice;
 
+import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
-@MessageDriven(mappedName="xx")
+import org.jboss.ejb3.annotation.ResourceAdapter;
+
+@MessageDriven(name = "DataExportQueueMDB", 
+	activationConfig = {
+		@ActivationConfigProperty(propertyName = "useJNDI", propertyValue = "false"),
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/queue/DataExportQueue"),
+        //@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/queue/DataExportQueue"),
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "user", propertyValue = "jmsuser"),
+        @ActivationConfigProperty(propertyName = "password", propertyValue = "jmspassword"),
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")
+    })
+@ResourceAdapter("remote-artemis")
 public class DataExportBean implements MessageListener {
 
 	@Override
