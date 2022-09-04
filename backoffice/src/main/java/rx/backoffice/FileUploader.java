2f373c3b41f432578a36e534ad344fcce89691e4
rui <ruixue.fi@gmail.com>
2022-09-04 22:57:38 +0300

    #37 multiple file upload mechanism works

--- backoffice/src/main/java/rx/backoffice/FileUploader.java
+++ backoffice/src/main/java/rx/backoffice/FileUploader.java
@@ -1,5 +1,8 @@
 package rx.backoffice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import javax.servlet.http.Part;
@@ -7,15 +10,21 @@ import javax.servlet.http.Part;
 @Named
 @RequestScoped
 public class FileUploader {
-	private Part file;
+	private List<Part> files = new ArrayList<>();
 	public void upload() {
-		System.out.println("going to UPLOAD FILE");
+		System.out.println("going to UPLOAD FILE " + files.get(0).getSubmittedFileName());
+		System.out.println("going to UPLOAD FILE " + files.get(1).getSubmittedFileName());
 	}
+	/**
+	 * NOTE! Declared merely due to the basic requirement of using manged bean in JSF application 
+	 * @return
+	 */
 	public Part getFile() {
-		return file;
+		return null;
 	}
 	public void setFile(Part file) {
-		this.file = file;
+		this.files.add(file);
 	}
+	
 	
 }
