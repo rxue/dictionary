a07cc5dec9b7bf9844387039d4d37c31ab554e4f
rui <ruixue.fi@gmail.com>
2022-10-06 22:10:51 +0300

    #49 embedded Wildfly still cannot work out-of-the-box

--- web/src/test/java/dictionary/ArquillianIT.java
+++ web/src/test/java/dictionary/ArquillianIT.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
@@ -14,16 +15,17 @@ import org.junit.runner.RunWith;
 
 import rx.dictionary.ExplanationRepository;
 import rx.dictionary.SearchKeyword;
-import rx.dictionary.util.Resources;
 
 @RunWith(Arquillian.class)
+@RunAsClient
 public class ArquillianIT {
 	@Deployment
     public static JavaArchive createDeployment() {
+		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!create deployment::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
         return ShrinkWrap.create(JavaArchive.class)
-        	.addPackage("rx.dictionary.jpaentity")
-            .addClasses(ExplanationRepository.class, Resources.class)
-            .addAsResource("persistence-IT.xml", "META-INF/persistence.xml")
+        	//.addPackage("rx.dictionary.jpaentity")
+            //.addClasses(ExplanationRepository.class, Resources.class)
+            //.addAsResource("persistence-I.xml", "META-INF/persistence.xml")
             .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 	
