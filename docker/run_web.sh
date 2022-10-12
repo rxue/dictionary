32732fec98e09c70b401e5d05c1d5727055322a0
rui <ruixue.fi@gmail.com>
2022-10-12 23:31:24 +0300

    #56 improved run_web.sh with a fake validation

--- docker/run_web.sh
+++ docker/run_web.sh
@@ -1,3 +1,11 @@
+#Validate: port 80 is not in use
+netstat -tulpn |grep :80
+if [ `echo $?` -eq 0 ]; then
+  echo 'NOTE!!! port 80 in use, so running of this docker container might fail if the port in use is not this existing Docker container'
+  sleep 5
+fi
+
+
 mvn -f ../web/pom.xml clean package
 docker-compose up -d --build web
 docker-compose logs -f web
