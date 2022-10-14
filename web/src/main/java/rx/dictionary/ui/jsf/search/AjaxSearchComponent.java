b6ef2c0464fc3c46d2366eed7846dd27f8e024a9
rui <ruixue.fi@gmail.com>
2022-10-14 07:41:54 +0300

    #56 select one item will invoke the backing bean method

--- web/src/main/java/rx/dictionary/ui/jsf/search/AjaxSearchComponent.java
+++ web/src/main/java/rx/dictionary/ui/jsf/search/AjaxSearchComponent.java
@@ -11,9 +11,13 @@ import javax.inject.Named;
 public class AjaxSearchComponent {
 	private String keyword;
 	private List<String> matchedResults;
+	private List<String> matchedResultsTest = Arrays.asList("first","fiiit");
 	public void fuzzySearch() {
 		System.out.println("::::::::::::::::!!!!!!!!!!!!!!!!!!!!!!!!!!! keyword is " + keyword);
-		matchedResults = Arrays.asList("x","y","z","K","a");
+		matchedResults = Arrays.asList("xxx","yyy","zzz","KKK","aaa");
+	}
+	public void search() {
+		System.out.println("real search::::::::::::::");
 	}
 	public String getKeyword() {
 		return keyword;
@@ -26,6 +30,13 @@ public class AjaxSearchComponent {
 	}
 	public void setMatchedResults(List<String> matchedResults) {
 		this.matchedResults = matchedResults;
+	}
+	
+	public List<String> getMatchedResultsTest() {
+		return matchedResultsTest;
+	}
+	public void setMatchedResultsTest(List<String> matchedResultsTest) {
+		this.matchedResultsTest = matchedResultsTest;
 	}
 	public int candidateSize() {
 		return 3;
