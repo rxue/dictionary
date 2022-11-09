fb607570c56d8de1811b67f8590c65f9138f1f31
rui <ruixue.fi@gmail.com>
2022-11-09 09:00:23 +0200

    #67 ci test code refactored to use only one docker container for the sake of simplicity

--- docker/run_ci_test_web.sh
+++ docker/run_ci_test_web.sh
@@ -1,11 +1,11 @@
 # Component test
-docker-compose up -d --build web-test-component
+docker-compose up -d --build web-test
+sleep 10
 mvn -f ../web/pom.xml clean verify -Parq-wildfly-remote
-docker-compose kill web-test-component
 # UAT
-docker-compose up -d --build web-test-uat
+docker cp ../web/target/dictionary.war dictionary-web-test:/opt/jboss/wildfly/standalone/deployments/
 sleep 10
 python3 -m unittest discover -s ../web/ci-test/uat
-docker-compose kill web-test-uat
+docker-compose kill web-test
 
 
