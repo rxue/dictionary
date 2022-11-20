c6a0558e50afac99a4226389c352c4f43730ad50
rui <ruixue.fi@gmail.com>
2022-11-20 12:18:49 +0200

    #78 verify added to execution goal to make Build fail
     - NOTE! compulsory to use source instead of bash to invoke the ci test bash script due to the use of return statement

--- docker/run_ci_test_web.sh
+++ docker/run_ci_test_web.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
 # Component test
 docker-compose up -d --build web-test
 sleep 10
+trap "docker-compose kill web-test" RETURN
 mvn -f ../web/pom.xml clean verify -Parq-wildfly-remote
 exit_code=$?
 if [ ${exit_code} -ne 0 ];
 then
-  docker-compose kill web-test
+  echo "Component test failed, and thus exit"
   return ${exit_code}
 fi
 # UAT
+echo "Start UAT"
 docker cp ../web/target/dictionary.war dictionary-web-test:/opt/jboss/wildfly/standalone/deployments/
 sleep 10
 python3 -m unittest discover -s ../web/ci-test/uat
 exit_code=$?
-docker-compose kill web-test
 return ${exit_code}
 
 
