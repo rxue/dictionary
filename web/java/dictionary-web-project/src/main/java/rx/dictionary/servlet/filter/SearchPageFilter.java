79479d97cd435d9f9408122c5489fbff7a1e6107
rui <ruixue.fi@gmail.com>
2023-06-17 10:56:39 +0300

    #95 improper use of filter detected

--- web/java/dictionary-web-project/src/main/java/rx/dictionary/servlet/filter/SearchPageFilter.java
+++ web/java/dictionary-web-project/src/main/java/rx/dictionary/servlet/filter/SearchPageFilter.java
@@ -10,14 +10,14 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 
-@WebFilter(filterName = "JSFFilter", urlPatterns = {"/en/zh-CN/search.xhtml"})
+@WebFilter(filterName = "JSFFilter", urlPatterns = {"/*"})
 public class SearchPageFilter implements Filter {
-
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {}
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		System.out.println(":::::::::::::::::::::::::::::filtering :)");
 		RequestDispatcher rd = request.getRequestDispatcher("../../search.xhtml");
 		rd.forward(request, response);
 		chain.doFilter(request, response);
