5cd225a22eb115e82570a6becbee221e2c06eba8
rui <ruixue.fi@gmail.com>
2023-09-27 23:26:57 +0300

    #110 grant access to dictionary on root user

--- docker/run_local_web.sh
+++ docker/run_local_web.sh
@@ -1,6 +1,7 @@
 docker-compose up -d mariadb
 sleep 5
 docker exec mariadb mysql -u root -ptest -e "CREATE DATABASE dictionary;"
+docker exec mariadb mysql -u root -ptest -e "GRANT ALL PRIVILEGES ON dictionary.* TO 'root'@'%' WITH GRANT OPTION;"
 jarURL="https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/3.2.0/mariadb-java-client-3.2.0.jar"
 jarName=`basename ${jarURL}`
 curl -o ../web/docker_config/${jarName} $jarURL
