cfe3f42c0d722d467ef6e88636d40cd3967329be
rui <ruixue.fi@gmail.com>
2023-09-27 22:18:15 +0300

    #110 successfully access to dicitionary database after netowork configuration fix

--- docker/run_local_web.sh
+++ docker/run_local_web.sh
@@ -1,5 +1,6 @@
 docker-compose up -d mariadb
 sleep 5
+docker exec mariadb mysql -u root -ptest -e "CREATE DATABASE dictionary;"
 jarURL="https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/3.2.0/mariadb-java-client-3.2.0.jar"
 jarName=`basename ${jarURL}`
 curl -o ../web/docker_config/${jarName} $jarURL
