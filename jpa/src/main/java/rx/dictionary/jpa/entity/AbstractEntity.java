683d61482b67fad174a800b7bfb984ece26252b3
rui <ruixue.fi@gmail.com>
2023-10-02 10:42:33 +0300

    #114 creation_date and last_update_time added

--- jpa/src/main/java/rx/dictionary/jpa/entity/AbstractEntity.java
+++ jpa/src/main/java/rx/dictionary/jpa/entity/AbstractEntity.java
@@ -1,20 +1,28 @@
 package rx.dictionary.jpa.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 @MappedSuperclass
 public abstract class AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected long id;
+    @Column(name="creation_date")
     private LocalDate creationDate;
+    @Column(name="last_update_time")
+    private ZonedDateTime lastUpdateTime;
 
+    @Column(name="creation_date")
     public LocalDate getCreationDate() {
         return creationDate;
+    }
+
+    @Column(name="last_update_time")
+    public ZonedDateTime getLastUpdateTime() {
+        return lastUpdateTime;
     }
 
     public void setId(long id) {
@@ -24,4 +32,18 @@ public abstract class AbstractEntity {
     public void setCreationDate(LocalDate creationDate) {
         this.creationDate = creationDate;
     }
+
+    public void setLastUpdateTime(ZonedDateTime lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    @PrePersist
+    public void setCreationDate() {
+        this.creationDate = LocalDate.now();
+    }
+    @PreUpdate
+    public void setLastUpdateTime() {
+        this.lastUpdateTime = ZonedDateTime.now();
+    }
+
 }
--- jpa/src/main/resources/import_mariadb.sql
+++ jpa/src/main/resources/import_mariadb.sql
@@ -1,4 +1,5 @@
-INSERT INTO lexical_item (language, value, creationdate) VALUES ('en','crux','2022-04-17 00:00:00');
+SET @current_dtime := NOW();
+INSERT INTO lexical_item (language, value, creation_date, last_update_time) VALUES ('en','crux','2022-04-17 00:00:00', @current_dtime);
 SET @last_lexical_item_id := LAST_INSERT_ID();
-INSERT INTO explanation (id, item_id, creationdate, language, partofspeech, explanation) VALUES (NEXTVAL(explanation_id_seq), @last_lexical_item_id, '2022-04-17 00:00:00','en','N','the most important point');
+INSERT INTO explanation (id, item_id, creation_date, last_update_time, language, partofspeech, explanation) VALUES (NEXTVAL(explanation_id_seq), @last_lexical_item_id, '2022-04-17 00:00:00', @current_dtime, 'en','N','the most important point');
 
