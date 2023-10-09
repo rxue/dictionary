0456e9738e436c9f3d3a0cc1623894b8a459b68c
rui <ruixue.fi@gmail.com>
2023-10-09 20:00:44 +0300

    #116 docker compose and relevant code updated to use compose V2

--- docker/web/deploy_2_wildfly.sh
+++ docker/web/deploy_2_wildfly.sh
@@ -2,8 +2,7 @@ jarURL="https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/3.2.
 jarName=`basename ${jarURL}`
 curl -o ../web/docker_config/${jarName} $jarURL
 mvn -f ../web/java/pom.xml clean package
-docker-compose up -d --build web
+docker compose up -d --build web
 source web/functions.sh
 waitBySleep 'docker-compose logs web |grep "Admin console listening on http://"'
-# docker-compose logs -f web
 
--- docker/web/start_mariadb.sh
+++ docker/web/start_mariadb.sh
@@ -1,9 +1,5 @@
-if [[ -n $(docker ps |grep mariadb:) ]]; then
-  # reference: https://www.baeldung.com/ops/docker-stop-vs-kill#:~:text=When%20we%20run%20the%20docker,a%20stopped%20or%20paused%20state.
-  docker stop mariadb
-  docker rm mariadb
-fi
-docker-compose up -d --remove-orphans mariadb
+docker compose down -v mariadb
+docker compose up -d --remove-orphans mariadb
 source web/functions.sh
 waitBySleep 'docker-compose logs mariadb |grep "mariadb.org binary distribution"'
 sleep 5
