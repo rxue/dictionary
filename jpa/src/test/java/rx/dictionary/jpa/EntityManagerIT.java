23619a347e95f0ce00f70819193521a4fba62f4e
rui <ruixue.fi@gmail.com>
2024-01-16 23:49:25 +0200

    #132 Explanation and LexicalItem entity's tables are created successfully

--- jpa/src/test/java/rx/dictionary/jpa/EntityManagerIT.java
+++ jpa/src/test/java/rx/dictionary/jpa/EntityManagerIT.java
@@ -1,13 +1,17 @@
 package rx.dictionary.jpa;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import rx.dictionary.jpa.entity.LexicalItem;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class EntityManagerTest extends AbstractDatabaseConfiguration {
+public class EntityManagerIT extends AbstractDatabaseConfiguration {
+
     @Test
     public void testEntity() {
         try (EntityManager em = entityManagerFactory.createEntityManager()) {
+            LexicalItem l = new LexicalItem();
+            em.persist(l);
             assertNotNull(em);
         }
     }
