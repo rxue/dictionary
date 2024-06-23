6dd1161b4e47f924395f7c0c266dcc9c8eaf4d63
rui <ruixue.fi@gmail.com>
2024-06-23 10:42:39 +0300

    #150 add method added to LexicalItemRepository

--- jpa/src/test/java/rx/dictionary/jpa/LexicalItemRepositoryCreateIT.java
+++ jpa/src/test/java/rx/dictionary/jpa/LexicalItemRepositoryCreateIT.java
@@ -1,37 +1,50 @@
 package rx.dictionary.jpa;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
-import org.junit.jupiter.api.AfterEach;
+
 import org.junit.jupiter.api.Test;
 import rx.dictionary.jpa.entity.Explanation;
 import rx.dictionary.jpa.entity.LexicalItem;
 import rx.dictionary.jpa.repository.LexicalItemRepository;
 
+import java.sql.ResultSet;
+import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
-import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static rx.dictionary.jpa.ITUtil.executeTransaction;
 
 public class LexicalItemRepositoryCreateIT extends AbstractDatabaseConfiguration {
-    /**
-    @AfterEach
-    public void truncate() {
-        try(EntityManager em = entityManagerFactory.createEntityManager()) {
-            EntityTransaction transaction = em.getTransaction();
-            transaction.begin();
-            em.createQuery("DELETE FROM Explanation")
-                    .executeUpdate();
-            em.createQuery("DELETE FROM LexicalItem")
-                    .executeUpdate();
-            transaction.commit();
-        }
+    private LexicalItem getLexicalItem() {
+        final LexicalItem existingSingleItem = executeTransactionWithReturnValue("select * from lexical_item", preparedStatement -> {
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    LexicalItem lexicalItem = new LexicalItem(resultSet.getLong("id"));
+                    lexicalItem.setLanguage(Locale.forLanguageTag(resultSet.getString("language")));
+                    lexicalItem.setValue(resultSet.getString("value"));
+                    return lexicalItem;
+                }
+                throw new IllegalArgumentException(preparedStatement + " does not return any result");
+            }
+        });
+        final Set<Explanation> explanations = executeTransactionWithReturnValue("select * from explanation where lexical_item_id = ?", preparedStatement -> {
+            preparedStatement.setLong(1, existingSingleItem.getId());
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                Set<Explanation> result = new HashSet<>();
+                while (resultSet.next()) {
+                    Explanation explanation = new Explanation(resultSet.getLong("id"));
+                    explanation.setLanguage(Locale.forLanguageTag(resultSet.getString("language")));
+                    explanation.setExplanation(resultSet.getString("explanation"));
+                    result.add(explanation);
+                }
+                return result;
+            }
+        });
+        explanations.forEach(existingSingleItem::addExplanation);
+        return existingSingleItem;
     }
 
     @Test
     public void testCreate() {
-        executeTransaction(entityManagerFactory, repo -> {
+        executeTransaction(entityManager -> {
                     LexicalItem l = new LexicalItem();
                     l.setLanguage(Locale.ENGLISH);
                     l.setValue("take");
@@ -39,51 +52,12 @@ public class LexicalItemRepositoryCreateIT extends AbstractDatabaseConfiguration
                     explanation.setLanguage(Locale.ENGLISH);
                     explanation.setExplanation("action of taking");
                     l.addExplanation(explanation);
-                    repo.add(l);
-                }
-        );
-        try (EntityManager em = entityManagerFactory.createEntityManager()) {
-            LexicalItem lexicalItem = ITUtil.getSingleLexicalItem(em);
-            assertAll("",
-                    () -> {
-                        assertNotNull(lexicalItem);
-                        //assertNotNull(lexicalItem.getId());
-                    },
-                    () -> {
-                        Set<Explanation> explanations = lexicalItem.getExplanations();
-                        Explanation explanation = explanations.stream().toList().get(0);
-                        assertNotNull(explanation);
-                        assertNotNull(explanation.getId());
-                    });
-        }
-    }
-    @Test
-    public void testAddMultipleExplanations() {
-        executeTransaction(entityManagerFactory, repo -> {
-            LexicalItem l = new LexicalItem();
-            l.setLanguage(Locale.ENGLISH);
-            l.setValue("take");
-            Explanation explanation = new Explanation();
-            explanation.setLanguage(Locale.ENGLISH);
-            explanation.setExplanation("action of taking");
-            l.addExplanation(explanation);
-            Explanation explanation2 = new Explanation();
-            explanation2.setLanguage(Locale.forLanguageTag("en"));
-            explanation2.setExplanation("action of taking 2");
-            l.addExplanation(explanation2);
-            repo.add(l);
+            LexicalItemRepository out = new LexicalItemRepository(entityManager);
+            out.add(l);
         });
-        try (EntityManager em = entityManagerFactory.createEntityManager()) {
-            LexicalItem lexicalItem = ITUtil.getSingleLexicalItem(em);
-            assertAll("",
-                    () -> {
-                        assertNotNull(lexicalItem);
-                        //assertNotNull(lexicalItem.getId());
-                    },
-                    () -> {
-                        Set<Explanation> explanations = lexicalItem.getExplanations();
-                        assertEquals(2, explanations.size());
-                    });
-        }
-    }*/
+        //ASSERT
+        final LexicalItem createdLexicalItem = getLexicalItem();
+        assertEquals("take", createdLexicalItem.getValue());
+        assertEquals("action of taking", createdLexicalItem.getExplanations().stream().findAny().get().getExplanation());
+    }
 }
