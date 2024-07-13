77cb9a7e9cb3c2b90617f0b375732818cdc8d1bc
rui <ruixue.fi@gmail.com>
2024-07-13 06:29:21 +0300

    #155 run CI test script (mostly in Bash) updated

--- docker/run_ci_test_web.sh
+++ docker/run_ci_test_web.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+# 1. start database (Docker container) and create users
+. web/start_mariadb.sh
+# 2. build schema and insert test data by running jpa project's main method
+mvn -f ../jpa exec:java -Dexec.mainClass="rx.dictionary.jpa.Main"
+# 3. build rest-api app war and deploy it to Wildfly
 # Component test
-docker compose up -d --build web-test
-sleep 10
-trap "docker-compose kill web-test" RETURN
-mvn -f ../jpa clean verify
-mvn -f ../web/java clean verify
-exit_code=$?
-if [ ${exit_code} -ne 0 ];
-then
-  echo "Component test failed, and thus exit"
-  return ${exit_code}
-fi
+docker compose up -d --build rest-api
+#sleep 10
+#trap "docker-compose kill web-test" RETURN
+#mvn -f ../jpa clean verify
+#mvn -f ../web/java clean verify
+#mvn -f ../jpa clean install -DskiptTests -DrunMainPhase=install
+##exit_code=$?
+#if [ ${exit_code} -ne 0 ];
+#then
+#  echo "Component test failed, and thus exit"
+#  return ${exit_code}
+#fi
 # UAT
 # echo "Start UAT"
 # docker cp ../web/java/web-project/target/web-project.war dictionary-web-test:/opt/jboss/wildfly/standalone/deployments/
