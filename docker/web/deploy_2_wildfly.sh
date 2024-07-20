a108eef5d53921e0b7076dd0251bd9470e1f51fa
rui <ruixue.fi@gmail.com>
2024-07-20 15:08:58 +0300

    #155 bug in deploy_2_wildfly.sh

--- docker/web/deploy_2_wildfly.sh
+++ docker/web/deploy_2_wildfly.sh
@@ -3,7 +3,6 @@ jarURL="https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/3.2.
 jarName=`basename ${jarURL}`
 curl -o ../web/docker_config/${jarName} $jarURL
 mvn -B -f ../web/java --projects rest-api --also-make clean package
-d-Djboss.http.port=ocker compose build --build-arg portAssignment="-Djboss.http.port=${REST_API_PORT}" rest-api
+docker compose build --build-arg portAssignment="-Djboss.http.port=${REST_API_PORT}" rest-api
 docker compose up -d rest-api
 waitBySleep 'docker logs dictionary-rest-api |grep "Admin console listening on http://"'
-npm --prefix=../web/ci-test/node test
