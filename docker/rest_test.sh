66c94d792d08f17e1c221dd4bf4c768d76e42a25
rui <ruixue.fi@gmail.com>
2024-07-20 15:29:58 +0300

    #155 renaming

---
+++ docker/rest_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source functions.sh ci_test.env
+# 1. start database (Docker container) and create users
+source web/start_mariadb.sh
+# 2. build schema and insert test data by running jpa project's main method
+mvn -B clean -f ../jpa install -DskipTests
+mvn -B -f ../jpa exec:java -Dexec.mainClass="rx.dictionary.jpa.Main"
+# 3. build REST API application and then deploy to Wildfly server
+source web/deploy_2_wildfly.sh
+npm --prefix=../web/ci-test/node install
+npm --prefix=../web/ci-test/node test
