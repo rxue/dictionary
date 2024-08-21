4e52fbed610e7b586953eeee24c0afc586fb7769
rui <ruixue.fi@gmail.com>
2024-08-21 22:18:31 +0300

    #162 waitBySleep function improved by adding wait interval

--- docker/functions.sh
+++ docker/functions.sh
@@ -2,13 +2,14 @@ env_file_name=${1}
 echo "env file: "${env_file_name}
 trap 'unset $(cat '${env_file_name}' |cut -d '=' -f 1 |xargs)' EXIT
 waitBySleep() {
+  local seconds=${1}
   while true; do
-    if [[ -n $(eval "$1") ]]; then
+    if [[ -n $(eval "$2") ]]; then
       echo "GREAT! got the needed terminate output"
       break
     else
-      echo "PROCESS - ${1} - IS NOT READY, GOING TO SLEEP"
-      sleep 1
+      echo "PROCESS - ${2} - IS NOT READY, GOING TO SLEEP"
+      sleep $seconds
     fi 
   done
 }
--- docker/web/start_mariadb.sh
+++ docker/web/start_mariadb.sh
@@ -3,6 +3,6 @@ source functions.sh ci_test.env
 echo "Run in environment "$ENV
 docker compose down -v mariadb
 docker compose up -d --remove-orphans mariadb
-waitBySleep 'docker logs '${DB_CONTAINER_NAME}' 2>&1 |grep "port: [0-9]\{4\}  mariadb.org binary distribution"'
+waitBySleep 3 'docker logs '${DB_CONTAINER_NAME}' 2>&1 |grep "port: [0-9]\{4\}  mariadb.org binary distribution"'
 docker exec ${DB_CONTAINER_NAME} mysql -u root -ptest -e "CREATE DATABASE IF NOT EXISTS dictionary;"
 docker exec ${DB_CONTAINER_NAME} mysql -u root -ptest -e "GRANT ALL PRIVILEGES ON dictionary.* TO 'root'@'%' WITH GRANT OPTION;"
