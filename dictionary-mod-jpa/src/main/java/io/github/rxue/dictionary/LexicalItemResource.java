c8b7ab11fbb5cc7e7ad7cab755e628bde37c99de
Rui Xue <rui.xue@fasolutions.com>
2025-05-31 10:24:59 +0300

    #176 endpoint created

--- dictionary-mod-jpa/src/main/java/io/github/rxue/dictionary/LexicalItemResource.java
+++ dictionary-mod-jpa/src/main/java/io/github/rxue/dictionary/LexicalItemResource.java
@@ -1,14 +1,14 @@
 package io.github.rxue.dictionary;
 
+import io.github.rxue.dictionary.jpa.entity.Explanation;
 import io.github.rxue.dictionary.jpa.entity.LexicalItem;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
+import java.util.Locale;
 
 @Path("/lexical-items")
 public class LexicalItemResource {
@@ -21,5 +21,19 @@ public class LexicalItemResource {
         List<LexicalItem> lexicalItems = entityManager.createQuery("SELECT l FROM LexicalItem l", LexicalItem.class)
                 .getResultList();
         return lexicalItems;
+    }
+
+    @Path("{word}/explanations")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Explanation> getExplanationsByLanguage(@PathParam("word") String word,
+                                                       @MatrixParam("language") String languageLocaleTag,
+                                                       @QueryParam("explanation-language") Locale explanationLanguageLocaleTag) {
+        System.out.println("word: " + word);
+        System.out.println("language: " + languageLocaleTag);
+        System.out.println("explanation in language Locale " + explanationLanguageLocaleTag);
+        List<Explanation> explanations = entityManager.createQuery("SELECT e FROM Explanation e", Explanation.class)
+                .getResultList();
+        return explanations;
     }
 }
