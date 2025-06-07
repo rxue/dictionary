feaa5d3bcabd6eff88897e8b507dbce7fff03cff
Rui Xue <rui.xue@fasolutions.com>
2025-06-07 21:59:23 +0300

    #185 end-to-end test template with nodejs added

---
+++ dictionary-mod-jpa/test/api.test.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const url = 'http://localhost:8080';
+const config = {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+
+describe('Error case', () => {
+  it('get get Simplified Chinese explanation of US English word, curate', async () => {
+    try {
+      const response = await axios.get(url + "/lexical-items/curate;language=en-US/explanations?language=zh-CN", config);
+      expect(response.status).toBe(200);
+      expect(response.data).not.toBeNull();
+    } catch(error) {
+      expect(error.response.status).toBe(422);
+    }
+  });
+});
+
+
