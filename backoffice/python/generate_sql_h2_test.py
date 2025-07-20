bd31a140479322d4144e7813c6650e329576cc17
Rui Xue <rui.xue@fasolutions.com>
2025-07-20 14:30:33 +0300

    #194 preparation: refactor existing code for better readability

--- backoffice/python/generate_sql_h2_test.py
+++ backoffice/python/generate_sql_h2_test.py
@@ -3,21 +3,28 @@ import unittest
 
 class TestGenerateSQL(unittest.TestCase):
     def test_getFieldValue_nextval(self):
-        input='NEXTVAL(seq)'
-        self.assertEqual(generate_sql_h2._getFieldValue(input), 'NEXT VALUE FOR seq')
+        self.assertEqual(
+            generate_sql_h2._getFieldValue('NEXTVAL(seq)'), 
+            'NEXT VALUE FOR seq')
     def test_getFieldValue_date(self):
-        input='2021-01-02'
-        self.assertEqual(generate_sql_h2._getFieldValue(input), 'PARSEDATETIME(\'2021-01-02\',\'yyyy-MM-dd\')')
+        self.assertEqual(
+            generate_sql_h2._getFieldValue('2021-01-02'),
+            "PARSEDATETIME('2021-01-02','yyyy-MM-dd')")
     def test_getFieldValue_currval(self):
-        input='LASTVAL(seq)'
-        self.assertEqual(generate_sql_h2._getFieldValue(input), 'seq.CURRVAL')
+        self.assertEqual(
+            generate_sql_h2._getFieldValue('LASTVAL(seq)'), 
+            'seq.CURRVAL')
     def test_getFieldValue_normalString(self):
-        input='value'
-        self.assertEqual(generate_sql_h2._getFieldValue(input), '\'value\'')
+        self.assertEqual(
+            generate_sql_h2._getFieldValue('value'), 
+            "'value'")
       
     def testGetInsertScript(self):
         row={'lexical_item.id':'NEXTVAL(item_id_seq)', 'lexical_item.createdate':'2023-01-09', 'lexical_item.language':'en'}
-        self.assertEqual(generate_sql_h2._getInsertScript(row), 'INSERT INTO lexical_item (id, createdate, language) VALUES (NEXT VALUE FOR item_id_seq,PARSEDATETIME(\'2023-01-09\',\'yyyy-MM-dd\'),\'en\');')
+        self.assertEqual(
+            generate_sql_h2._getInsertScript(row), 
+            "INSERT INTO lexical_item (id, createdate, language) VALUES (NEXT VALUE FOR item_id_seq,PARSEDATETIME('2023-01-09','yyyy-MM-dd'),'en');"
+            )
 
 if __name__ == '__main__':
    unittest.main()
