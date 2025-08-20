72ca9c96f5abb2d43b99ad341e2bc13bc0a6693e
Rui Xue <ruixue.fi@gmail.com>
2025-08-20 20:51:18 +0300

    #197 csvrowconverter improved

--- backoffice/python/src/backoffice/csvrowconverter.py
+++ backoffice/python/src/backoffice/csvrowconverter.py
@@ -26,65 +26,83 @@ def _quote(value:str):
         return value
     return "'" + value + "'"
 
-def _to_using_clause(single_table_record:dict, alias:str) -> str:
-        def to_as_clause(column:str, value:str):
+def _table_name(single_table_record: dict[str,str]) -> str:
+    return get_table_name(next(iter(single_table_record)))
+
+def _is_composite_key(header_column: str) -> bool:
+    return header_column.startswith("*")
+
+def _to_using_on_clause(single_table_record:dict, cte_prefix:str, alias:str) -> tuple[str,str]:
+    """
+    :param single_table_record:
+    :param alias:
+    :return: USING ..., ON ...
+    """
+    def _to_select_clause() -> str:
+        def _as_clause(column:str, value:str) -> str:
             if is_foreign_key(column):
-                foreign_key = ForeignKey(column)
-                return 'merged_' + foreign_key.reference() + " AS " + table_column_name(column)
-            return _quote(value) + " AS " + table_column_name(column)
-        as_list = [to_as_clause(c, v) for c, v in single_table_record.items() if c.startswith('*')]
-        return "USING (SELECT " + ','.join(as_list) + ") AS " + alias
+                return ForeignKey(column).reference_column() + ' AS ' + table_column_name(column)
+            else:
+                return _quote(value) + ' AS ' + table_column_name(column)
+        as_list = [ _as_clause(c,v) for c,v in single_table_record.items() if _is_composite_key(c)]
+        foreign_keys = [ForeignKey(c) for c, v in single_table_record.items() if is_foreign_key(c)]
+        if len(foreign_keys) > 0:
+            reference_tables = [cte_prefix + f.reference_table() for f in foreign_keys]
+            return "SELECT " + ','.join(as_list) + ' FROM ' + ','.join(reference_tables)
+        return "SELECT " + ','.join(as_list)
+    def _on_condition() -> str:
+        table_name = _table_name(single_table_record)
+        condition = [table_name + '.' + table_column_name(c) + " = u." + table_column_name(c) for c,v in single_table_record.items() if _is_composite_key(c)]
+        return ' AND '.join(condition)
+    return "USING (" + _to_select_clause() + ") AS " + alias, 'ON ' + _on_condition()
 
 def _get_unique_columns(single_table_record:dict) -> list[str]:
     return [table_column_name(c) for c, v in single_table_record.items() if c.startswith("*")]
 
-def _on_clause(single_table_record:dict, table_name:str) -> str:
-    on_condition_list = [ table_name + "." + c + " = u." + c for c in _get_unique_columns(single_table_record)]
-    return 'ON ' + ' AND '.join(on_condition_list)
-
 def _to_update_clause(single_table_record: dict[str,str]) -> str:
-    set_list = [table_column_name(c) + " = " + _quote(v) for c, v in single_table_record.items() if not is_foreign_key(c)]
+    set_list = [table_column_name(c) + " = " + _quote(v) for c, v in single_table_record.items() if not _is_composite_key(c)]
     return "UPDATE SET " + ','.join(set_list)
 
-def _to_insert_clause(single_table_record: dict[str,str]) -> str:
+def _to_insert_clause(single_table_record: dict[str,str], reference_cte_prefix:str) -> str:
     def get_columns() -> str:
         column_list = [table_column_name(c) for c in single_table_record.keys()]
         return ','.join(column_list)
     def get_value(column:str,value:str) -> str:
         if is_foreign_key(column):
             foreign_key = ForeignKey(column)
-            return 'merged_' + foreign_key.reference()
+            return reference_cte_prefix + foreign_key.reference()
         return _quote(value)
     value_list = [get_value(c,v) for c,v in single_table_record.items()]
     values = ','.join(value_list)
-    return "INSERT INTO (" + get_columns() + ") VALUES(" + values + ")"
+    return "INSERT (" + get_columns() + ") VALUES(" + values + ")"
 
 
-def _to_merge_statement(single_table_row_record:dict, returning_value:str=None) -> str:
+def _to_merge_statement(single_table_row_record:dict, reference_cte_prefix:str, returning_value:str=None) -> str:
     statement_rows = []
-    table_name = get_table_name(next(iter(single_table_row_record)))
+    table_name = _table_name(single_table_row_record)
     statement_rows.append("MERGE INTO " + table_name)
-    statement_rows.append(_to_using_clause(single_table_row_record, 'u'))
-    statement_rows.append("    " + _on_clause(single_table_row_record, table_name))
+    using_on_clause = _to_using_on_clause(single_table_row_record, reference_cte_prefix, 'u')
+    statement_rows.append(using_on_clause[0])
+    statement_rows.append("    " + using_on_clause[1])
     statement_rows.append("WHEN MATCHED THEN")
     statement_rows.append("    " + _to_update_clause(single_table_row_record))
     if returning_value is not None:
         statement_rows.append("    RETURNING " + returning_value)
     statement_rows.append("WHEN NOT MATCHED THEN")
-    statement_rows.append("    " + _to_insert_clause(single_table_row_record))
+    statement_rows.append("    " + _to_insert_clause(single_table_row_record, reference_cte_prefix))
     if returning_value is not None:
         statement_rows.append("    RETURNING " + returning_value)
     return '\n'.join(statement_rows) + ';'
 
 
-class RowParser:
+class RowConverter:
     def __init__(self, row:dict[str,str], foreign_keys:list[ForeignKey]):
         self.row = row
         self.foreign_keys = foreign_keys
-    def parse_row(self) -> list[str]:
+    def convert(self) -> list[str]:
         """
         :param row:
-        :return: list of SQL merge statements. NOTE that merge statements corresponding to one csv row have dependencies on each other
+        :return: list of SQL merge statements. NOTE that statements corresponding to one csv row have dependencies on each other
         """
-        divided_row_input_list = _divide(self.row, self.foreign_keys)
+        row_input_list = _divide(self.row, self.foreign_keys)
 
--- backoffice/python/tests/importsql/test_csvrowconverter.py
+++ backoffice/python/tests/importsql/test_csvrowconverter.py
@@ -3,7 +3,7 @@ import unittest
 from backoffice.headerparser import ForeignKey
 
 
-class CSVRowParserTest(unittest.TestCase):
+class CSVRowConverterUtilityFunctionsTest(unittest.TestCase):
     def test__divide(self):
         from backoffice.csvrowconverter import _divide
         foreign_key_column = '*explanation.lexical_item_id (lexical_item.id)'
@@ -29,32 +29,58 @@ class CSVRowParserTest(unittest.TestCase):
             'explanation.definition': 'pin yin'}, None)
         self.assertEqual([expected_result1, expected_result2], _divide(row_input, foreign_keys))
 
-    def test__to_using_clause_without_foreign_key(self):
-        from backoffice.csvrowconverter import _to_using_clause
-        key_value_pairs = {'*lexical_item.language':'en_US', '*lexical_item.value':'test', 'explanation.definition':'test'}
-        self.assertEqual("USING (SELECT 'en_US' AS language,'test' AS value) AS u", _to_using_clause(key_value_pairs, 'u'))
-    def test__to_using_clause_with_foreign_key(self):
-        from backoffice.csvrowconverter import _to_using_clause
-        key_value_pairs = {'*explanation.lexical_item_id (lexical_item.id)':'', '*explanation.language':'en_US', '*explanation.partofspeech':'N', '*explanation.serial_number':'1', 'explanation.definition':'test'}
-        self.assertEqual("USING (SELECT merged_lexical_item.id AS lexical_item_id,'en_US' AS language,'N' AS partofspeech,'1' AS serial_number) AS u", _to_using_clause(key_value_pairs, 'u'))
-
-    def test__on_clause(self):
-        from backoffice.csvrowconverter import _on_clause
-        key_value_pairs = {'*lexical_item.language': 'en_US', '*lexical_item.value': 'test'}
-        self.assertEqual("ON lexical_item.language = u.language AND lexical_item.value = u.value", _on_clause(key_value_pairs, 'lexical_item'))
+    def test__to_using_on_clause_without_foreign_key(self):
+        from backoffice.csvrowconverter import _to_using_on_clause
+        key_value_pairs = {'*lexical_item.language':'en_US', '*lexical_item.value':'test'}
+        expected_using_clause = ("USING (SELECT 'en_US' AS language,'test' AS value) AS u",
+                                 "ON lexical_item.language = u.language AND lexical_item.value = u.value")
+        self.assertEqual(expected_using_clause, _to_using_on_clause(key_value_pairs, 'merged_', 'u'))
+    def test__to_using_on_clause_with_foreign_key(self):
+        from backoffice.csvrowconverter import _to_using_on_clause
+        key_value_pairs = {'*explanation.lexical_item_id (lexical_item.id)':'', '*explanation.language':'en_US', '*explanation.partofspeech':'N', '*explanation.serialnumber':'1', 'explanation.definition':'test'}
+        expected_using_clause = ("USING (SELECT id AS lexical_item_id,'en_US' AS language,'N' AS partofspeech,'1' AS serialnumber FROM merged_lexical_item) AS u",
+                                 "ON explanation.lexical_item_id = u.lexical_item_id AND explanation.language = u.language AND explanation.partofspeech = u.partofspeech AND explanation.serialnumber = u.serialnumber")
+        self.assertEqual(expected_using_clause, _to_using_on_clause(key_value_pairs, 'merged_', 'u'))
 
     def test__to_merge_statement(self):
         from backoffice.csvrowconverter import _to_merge_statement
         key_value_pairs = {
             '*explanation.lexical_item_id (lexical_item.id)': '',
+            '*explanation.language': 'en',
+            '*explanation.partofspeech': 'N',
+            '*explanation.serialnumber': '1',
+            'explanation.definition': 'test',
+            'explanation.last_update_time': '2025-08-15 00:00:00+00'}
+        expected_statement = """MERGE INTO explanation
+USING (SELECT id AS lexical_item_id,'en' AS language,'N' AS partofspeech,'1' AS serialnumber FROM merged_lexical_item) AS u
+    ON explanation.lexical_item_id = u.lexical_item_id AND explanation.language = u.language AND explanation.partofspeech = u.partofspeech AND explanation.serialnumber = u.serialnumber
+WHEN MATCHED THEN
+    UPDATE SET definition = 'test',last_update_time = '2025-08-15 00:00:00+00'
+WHEN NOT MATCHED THEN
+    INSERT (lexical_item_id,language,partofspeech,serialnumber,definition,last_update_time) VALUES(merged_lexical_item.id,'en','N','1','test','2025-08-15 00:00:00+00');"""
+        self.assertEqual(expected_statement, _to_merge_statement(key_value_pairs, 'merged_'))
+
+class RowConverterTest(unittest.TestCase):
+    def test_convert(self):
+        from backoffice.csvrowconverter import RowConverter
+        foreign_key_column = '*explanation.lexical_item_id (lexical_item.id)'
+        row_input = {
+            '*lexical_item.language': 'en',
+            '*lexical_item.value': 'test',
+            'lexical_item.last_update_time': '2025-08-09',
+            foreign_key_column: '',
+            '*explanation.language': 'zh_CN',
             '*explanation.partofspeech': 'N',
             '*explanation.serial_number': '1',
-            'explanation.definition': 'test'}
-        expected_statement = """MERGE INTO explanation
-USING (SELECT merged_lexical_item.id AS lexical_item_id,'N' AS partofspeech,'1' AS serial_number) AS u
-    ON explanation.lexical_item_id = u.lexical_item_id AND explanation.partofspeech = u.partofspeech AND explanation.serial_number = u.serial_number
+            'explanation.definition': 'pin yin'}
+        foreign_keys = [ForeignKey(foreign_key_column)]
+        rowConverter = RowConverter(row_input, foreign_keys)
+        converted_statements = rowConverter.convert()
+        self.assertEqual(2, len(converted_statements))
+        expected_statement1 = """MERGE INTO lexical_item
+USING (SELECT 'en' AS language,'value' AS value) AS u
+    ON lexical_item.language = u.language AND lexical_item.value = u.value
 WHEN MATCHED THEN
     UPDATE SET partofspeech = 'N',serial_number = '1',definition = 'test'
 WHEN NOT MATCHED THEN
     INSERT INTO (lexical_item_id,partofspeech,serial_number,definition) VALUES(merged_ lexical_item.id,'N','1','test');"""
-        self.assertEqual(expected_statement, _to_merge_statement(key_value_pairs))
