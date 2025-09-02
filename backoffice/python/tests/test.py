07ad8dea365b27c6c055da432e3c0c0fa7dc683f
Rui Xue <ruixue.fi@gmail.com>
2025-09-02 07:51:59 +0300

    #203 update the test data with many tables so that there are multiple CTE

--- backoffice/python/tests/test.py
+++
@@ -1,6 +0,0 @@
-import csv
-if __name__ == "__main__":
-    with open('explanations.csv', mode='r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            print(row['*explanation.lexical_item_id (lexical_item.id)'])
