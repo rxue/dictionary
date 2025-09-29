0e3ae3b8c1abe782d7318b1027783a046ff9002e
Rui Xue <ruixue.fi@gmail.com>
2025-09-29 22:24:40 +0300

    #208 made IT test use the application.properties as the first step

--- dictionary-rest-api/src/test/java/io/github/rxue/PostgresResource.java
+++ dictionary-rest-api/src/test/java/io/github/rxue/PostgresResource.java
@@ -2,6 +2,8 @@ package io.github.rxue;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.util.HashMap;
@@ -10,14 +12,19 @@ import java.util.Map;
 @QuarkusTestResource(PostgresResource.class)
 public class PostgresResource implements QuarkusTestResourceLifecycleManager {
 
-    private PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>("postgres:15")
-                    .withDatabaseName("testdb")
-                    .withUsername("test")
-                    .withPassword("test");
+    private PostgreSQLContainer<?> postgres;
 
     @Override
     public Map<String, String> start() {
+        Config config = ConfigProvider.getConfig();
+
+        final String dbName = config.getOptionalValue("quarkus.datasource.database", String.class).orElse("defaulttestdb");
+        final String username = config.getOptionalValue("quarkus.datasource.username", String.class).orElse("defaultuser");
+        final String password = config.getOptionalValue("quarkus.datasource.password", String.class).orElse("defaultpassword");
+        postgres = new PostgreSQLContainer<>("postgres:15")
+                        .withDatabaseName(dbName)
+                        .withUsername(username)
+                        .withPassword(password);
         postgres.start();
         Map<String, String> props = new HashMap<>();
         props.put("quarkus.datasource.jdbc.url", postgres.getJdbcUrl());
