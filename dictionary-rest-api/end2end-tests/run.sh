90e3a86c188e42ca099d7e354b11a13a62413abb
Rui Xue <ruixue.fi@gmail.com>
2025-09-30 21:52:02 +0300

    #208 added quatkus.telemetry=true to avoid asking yes or not when running quarkus app

--- dictionary-rest-api/end2end-tests/run.sh
+++ dictionary-rest-api/end2end-tests/run.sh
@@ -1,4 +1,12 @@
-mvn -f ../pom.xml quarkus:dev &
+#docker run -d \
+#  --name my-postgres \
+#  -e POSTGRES_USER=test \
+#  -e POSTGRES_PASSWORD=test \
+#  -e POSTGRES_DB=testdb \
+#  -p 15:5432 \
+#  postgres:17 &
+#sleep 20
+mvn -f ../pom.xml quarkus:dev -Dquarkus.telemetry=true &
 pid=$!
 sleep 20
 npm install
